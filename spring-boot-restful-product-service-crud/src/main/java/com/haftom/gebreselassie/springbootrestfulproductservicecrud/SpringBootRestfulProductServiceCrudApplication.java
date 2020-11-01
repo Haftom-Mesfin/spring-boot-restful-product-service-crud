46c5b4dc3d12d1dbefdada86f2624031d2114211
Haftom-Mesfin <sadtime1906@gmail.com>
2020-11-01 14:05:18 -0600

    First commit.

---
+++ spring-boot-restful-product-service-crud/src/main/java/com/haftom/gebreselassie/springbootrestfulproductservicecrud/SpringBootRestfulProductServiceCrudApplication.java
@@ -0,0 +1,13 @@
+package com.haftom.gebreselassie.springbootrestfulproductservicecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulProductServiceCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulProductServiceCrudApplication.class, args);
+	}
+
+}
---
+++ spring-boot-restful-product-service-crud/src/test/java/com/haftom/gebreselassie/springbootrestfulproductservicecrud/SpringBootRestfulProductServiceCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haftom.gebreselassie.springbootrestfulproductservicecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulProductServiceCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
