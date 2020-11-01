83f9e88b855d2a1a539c699cc4c8192dc7c8bd76
Haftom-Mesfin <sadtime1906@gmail.com>
2020-11-01 14:41:50 -0600

    reposiroty created

---
+++ spring-boot-restful-product-service-crud/src/main/java/com/haftom/gebreselassie/springbootrestfulproductservicecrud/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.haftom.gebreselassie.springbootrestfulproductservicecrud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.haftom.gebreselassie.springbootrestfulproductservicecrud.domain.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
